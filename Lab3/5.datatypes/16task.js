a5b9b38b704a4bcad353b2265a6b86fd62a3b0ef
Zhantoresh <z_orazymbetov@kbtu.kz>
2025-02-07 15:14:14 +0600

    js

--- Lab3/5.datatypes/16task.js
+++ Lab3/5.datatypes/16task.js
@@ -4,3 +4,30 @@ let user = {
 };
 
 let user2 = JSON.parse(JSON.stringify(user));
+
+let room = {
+  number: 23,
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
+  place: room,
+};
+
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(
+  JSON.stringify(meetup, function replacer(key, value) {
+    return key != "" && value == meetup ? undefined : value;
+  })
+);
+
+/*
+  {
+    "title":"Совещание",
+    "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+    "place":{"number":23}
+  }
+  */
